564ef867bd50f2505035351d04549cb33b3b8342
HajarBoulmane <hajarboulmane2003@gmail.com>
2023-02-20 20:41:05 +0000

    navbar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>H&K</title>
+	</head>
+	<body>
+<header>
+<nav>
+	<ul>
+		<li>Home</li>
+		<li>Women</li>
+		<li>Men</li>
+		<li>Trend</li>
+		<li>About-us</li>
+		<li>Help</li>
+	</ul>
+</nav>
+</header>
+
+	</body>
+</html>
